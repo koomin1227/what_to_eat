29a5dcad801a78b7282ced95d24404f6b77193a8
koomin1227 <koomin1227@naver.com>
2024-05-14 00:43:50 +0900

    Feat: Tag model 구현

--- lib/models/tag.dart
+++ lib/models/tag.dart
@@ -6,4 +6,32 @@ class Tag {
     required this.name,
     required this.category,
   });
+
+  factory Tag.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+      'name': String name,
+      'category': String category,
+      } =>
+          Tag(
+            name: name,
+            category: category,
+          ),
+      _ => throw const FormatException('Failed to load Tag.'),
+    };
+  }
+
+  static List<Tag> listFromJson(Map<String, dynamic> json) {
+    return [
+      for (var element in json["tags"])
+        Tag.fromJson(element)
+    ];
+  }
+
+  static List<Tag> listFromJsonList(json) {
+    return [
+      for (var element in json)
+        Tag.fromJson(element)
+    ];
+  }
 }
