945137c5a583f574b8a47953ee327baa470c547c
koomin1227 <koomin1227@naver.com>
2024-05-14 01:05:15 +0900

    Feat: Restaurant model 구현

---
+++ lib/models/restaurant.dart
@@ -0,0 +1,40 @@
+import 'package:what_to_eat/models/tag.dart';
+
+class Restaurant {
+  final String name;
+  final String thumbnail;
+  final String restaurantId;
+  final List<Tag> tags;
+
+  Restaurant({
+    required this.name,
+    required this.thumbnail,
+    required this.restaurantId,
+    required this.tags,
+  });
+
+  factory Restaurant.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+      'name': String name,
+      'thumbnail': String thumbnail,
+      'restaurantId': String restaurantId,
+      'tags': var json,
+      } =>
+          Restaurant(
+            name: name,
+            thumbnail: thumbnail,
+            restaurantId: restaurantId,
+            tags: Tag.listFromJsonList(json),
+          ),
+      _ => throw const FormatException('Failed to load Restaurant.'),
+    };
+  }
+
+  static List<Restaurant> listFromJson(Map<String, dynamic> json) {
+    return [
+      for (var element in json["restaurants"])
+        Restaurant.fromJson(element)
+    ];
+  }
+}
