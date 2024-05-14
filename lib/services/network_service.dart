999711d4ff4ee98ea2e97f1145d8512e5ceae828
koomin1227 <koomin1227@naver.com>
2024-05-14 22:32:29 +0900

    Feat: getRestaurants 에서 페이지를 적용 할 수 있도록 추가

--- lib/services/network_service.dart
+++ lib/services/network_service.dart
@@ -3,17 +3,18 @@ import 'package:http/http.dart' as http;
 class NetworkService {
   static String baseURL = "http://43.202.161.19:8080/api";
 
-  static getRestaurants(String place) {
+  static getRestaurants(String place, int page) {
     var url = Uri.parse("$baseURL/restaurants/tag")
-        .replace(queryParameters: makeQueryOptions(place));
+        .replace(queryParameters: makeQueryOptions(place, page));
     return http.get(url);
   }
 
-  static Map<String, dynamic> makeQueryOptions(String place) {
+  static Map<String, dynamic> makeQueryOptions(String place, int page) {
     Map<String, dynamic> query = {};
     if (place != "전체") {
       query["place"] = place;
     }
+    query["page"] = page.toString();
     return query;
   }
 }
