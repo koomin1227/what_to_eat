406a807774a024ec9b2b48994f32ee88d7e9137f
koomin1227 <koomin1227@naver.com>
2024-05-20 23:33:19 +0900

    Refactor: 태그 선택 분리

--- lib/screens/restaurant_list_screen.dart
+++ lib/screens/restaurant_list_screen.dart
@@ -2,12 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:what_to_eat/services/network_service.dart';
 import 'package:what_to_eat/utils/data_extractor.dart';
-import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 import '../models/tag.dart';
-import '../models/restaurant.dart';
 import '../widgets/restaurant_list_screen/place_select_buttons_widget.dart';
-import '../widgets/restaurant_list_screen/restaurant_card_widget.dart';
 import '../widgets/restaurant_list_screen/restaurant_list_view_widget.dart';
 import '../widgets/restaurant_list_screen/restaurant_search_bar_widget.dart';
 
@@ -77,143 +74,7 @@ class RestaurantListScreenState extends State<RestaurantListScreen> {
           searchOption: searchOption,
           setPlace: setPlace,
         ),
-        Container(
-          padding: EdgeInsets.fromLTRB(8.0, 10.0, 8.0, 8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              if (_expanded)
-                Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: Text("# 태그",
-                          style: TextStyle(
-                              color: Theme.of(context).colorScheme.primary,
-                              fontWeight: FontWeight.bold)),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                  ],
-                ),
-              AnimatedContainer(
-                duration: Duration(milliseconds: 300),
-                curve: Curves.easeInOut,
-                constraints: BoxConstraints(maxHeight: _expanded ? 600 : 25),
-                child: ClipRect(
-                  child: Wrap(
-                    direction: Axis.horizontal,
-                    alignment: WrapAlignment.center,
-                    spacing: 10,
-                    runSpacing: 10,
-                    children: [
-                      for (String tag in tags)
-                        SizedBox(
-                          height: 25,
-                          child: TextButton(
-                            style: TextButton.styleFrom(
-                              backgroundColor: searchOption.selectedTags
-                                      .contains(tag)
-                                  ? Theme.of(context).colorScheme.primary
-                                  : Theme.of(context).colorScheme.background,
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 0, horizontal: 4),
-                              side: BorderSide(
-                                  color: searchOption.selectedTags.contains(tag)
-                                      ? Theme.of(context).colorScheme.primary
-                                      : Colors.grey),
-                            ),
-                            onPressed: () {
-                              if (searchOption.selectedTags.contains(tag)) {
-                                searchOption.selectedTags.remove(tag);
-                              } else {
-                                searchOption.selectedTags.add(tag);
-                              }
-                              setState(() {});
-                            },
-                            child: Text(
-                              "# ${tag}",
-                              style: TextStyle(
-                                  color: searchOption.selectedTags.contains(tag)
-                                      ? Theme.of(context).colorScheme.background
-                                      : Colors.black),
-                            ),
-                          ),
-                        ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          SizedBox(
-                            width: 160,
-                            height: 35,
-                            child: TextButton(
-                                style: TextButton.styleFrom(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(8),
-                                        side: BorderSide(color: Colors.grey)),
-                                    padding: const EdgeInsets.symmetric(
-                                        vertical: 0, horizontal: 4),
-                                    backgroundColor: Theme.of(context)
-                                        .colorScheme
-                                        .background),
-                                onPressed: () {
-                                  setState(() {
-                                    searchOption.selectedTags.clear();
-                                  });
-                                },
-                                child: Text(
-                                  "초기화",
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 15),
-                                )),
-                          ),
-                          // SizedBox(width: 10,),
-                          SizedBox(
-                            width: 160,
-                            height: 35,
-                            child: TextButton(
-                                style: TextButton.styleFrom(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(8)),
-                                    padding: const EdgeInsets.symmetric(
-                                        vertical: 0, horizontal: 4),
-                                    backgroundColor:
-                                        Theme.of(context).colorScheme.primary),
-                                onPressed: () {
-                                  setState(() {
-                                    searchOption.isSearch = false;
-                                    searchOption.changeStatus();
-                                  });
-                                  _toggleExpand();
-                                },
-                                child: Text(
-                                  "결과보기",
-                                  style: TextStyle(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .background,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 15),
-                                )),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
+        TagSelector(),
         Center(
           child: Column(
             children: [
@@ -256,7 +117,167 @@ class RestaurantListScreenState extends State<RestaurantListScreen> {
 }
 
 
+class TagSelector extends StatefulWidget {
 
+  final SearchOption searchOption;
+
+  const TagSelector({super.key, required this.searchOption});
+
+  @override
+  State<TagSelector> createState() => _TagSelectorState();
+}
+
+class _TagSelectorState extends State<TagSelector> {
+  List<String> tags = [];
+  bool _expanded = false;
+
+  void _toggleExpand() {
+    setState(() {
+      _expanded = !_expanded;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(8.0, 10.0, 8.0, 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (_expanded)
+            Column(
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text("# 태그",
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontWeight: FontWeight.bold)),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+              ],
+            ),
+          AnimatedContainer(
+            duration: Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+            constraints: BoxConstraints(maxHeight: _expanded ? 600 : 25),
+            child: ClipRect(
+              child: Wrap(
+                direction: Axis.horizontal,
+                alignment: WrapAlignment.center,
+                spacing: 10,
+                runSpacing: 10,
+                children: [
+                  for (String tag in tags)
+                    SizedBox(
+                      height: 25,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: searchOption.selectedTags
+                              .contains(tag)
+                              ? Theme.of(context).colorScheme.primary
+                              : Theme.of(context).colorScheme.background,
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 0, horizontal: 4),
+                          side: BorderSide(
+                              color: searchOption.selectedTags.contains(tag)
+                                  ? Theme.of(context).colorScheme.primary
+                                  : Colors.grey),
+                        ),
+                        onPressed: () {
+                          if (searchOption.selectedTags.contains(tag)) {
+                            searchOption.selectedTags.remove(tag);
+                          } else {
+                            searchOption.selectedTags.add(tag);
+                          }
+                          setState(() {});
+                        },
+                        child: Text(
+                          "# ${tag}",
+                          style: TextStyle(
+                              color: searchOption.selectedTags.contains(tag)
+                                  ? Theme.of(context).colorScheme.background
+                                  : Colors.black),
+                        ),
+                      ),
+                    ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SizedBox(
+                        width: 160,
+                        height: 35,
+                        child: TextButton(
+                            style: TextButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                    side: BorderSide(color: Colors.grey)),
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 0, horizontal: 4),
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .background),
+                            onPressed: () {
+                              setState(() {
+                                searchOption.selectedTags.clear();
+                              });
+                            },
+                            child: Text(
+                              "초기화",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15),
+                            )),
+                      ),
+                      // SizedBox(width: 10,),
+                      SizedBox(
+                        width: 160,
+                        height: 35,
+                        child: TextButton(
+                            style: TextButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8)),
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 0, horizontal: 4),
+                                backgroundColor:
+                                Theme.of(context).colorScheme.primary),
+                            onPressed: () {
+                              setState(() {
+                                searchOption.isSearch = false;
+                                searchOption.changeStatus();
+                              });
+                              _toggleExpand();
+                            },
+                            child: Text(
+                              "결과보기",
+                              style: TextStyle(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .background,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15),
+                            )),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
 
 
 
