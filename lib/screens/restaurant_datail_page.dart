7448927f19d5fb40375bdb34f87a22ac77826a9a
koomin1227 <koomin1227@naver.com>
2024-05-20 15:40:45 +0900

    Feat: 상세 페이지 클릭시 서버로 부터 데이터 받아오는 기능 구현

--- lib/screens/restaurant_datail_page.dart
+++ lib/screens/restaurant_datail_page.dart
@@ -1,13 +1,66 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:what_to_eat/services/network_service.dart';
+import 'package:what_to_eat/utils/data_extractor.dart';
 
-class RestaurantDetailPage extends StatelessWidget {
+import '../models/restaurantDetail.dart';
+
+class RestaurantDetailPage extends StatefulWidget {
+  final String restaurantId;
+
+  const RestaurantDetailPage({super.key, required this.restaurantId});
+
+  @override
+  State<RestaurantDetailPage> createState() => _RestaurantDetailPageState();
+}
+
+class _RestaurantDetailPageState extends State<RestaurantDetailPage> {
+  late Future<RestaurantDetail> restaurantDetail;
+
+  Future<RestaurantDetail> fetchData() async {
+    Response res = await NetworkService.getRestaurantDetail(widget.restaurantId);
+    if (res.statusCode == 404) {
+      throw Exception('No data found for this restaurant');
+    }
+    RestaurantDetail restaurantDetail = RestaurantDetail.fromJson(DataExtractor.extractData(res));
+    return restaurantDetail;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    restaurantDetail = fetchData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
-        body: Text("hello")
+        body: FutureBuilder<RestaurantDetail>(
+          future: restaurantDetail,
+          builder: (BuildContext context, AsyncSnapshot<RestaurantDetail> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else if (snapshot.hasData) {
+              var restaurant = snapshot.data!;
+              return Column(
+                children: [
+                  Image.network(restaurant.thumbnail),
+                  Text('Name: ${restaurant.name}'),
+                  Text('ID: ${restaurant.restaurantId}'),
+                  Text('Tags: ${restaurant.tags.join(', ')}'),
+                ],
+              );
+            } else {
+              return Text('No data');
+            }
+          },
+
+
+        ),
     );
   }
-
 }
