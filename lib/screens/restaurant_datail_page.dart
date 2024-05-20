3327de9c3ac8169de2716572e9b3636114927a66
koomin1227 <koomin1227@naver.com>
2024-05-20 18:33:20 +0900

    Feat: 공유 버튼, 지도 버튼 추가

--- lib/screens/restaurant_datail_page.dart
+++ lib/screens/restaurant_datail_page.dart
@@ -74,59 +74,151 @@ class _RestaurantDetailPageState extends State<RestaurantDetailPage>
                   padding: EdgeInsets.all(15),
                   child: Column(
                     children: [
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          restaurant.name,
-                          style: TextStyle(
-                              fontSize: 25, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Rating(),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Text(
-                            '리뷰 157개',
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          )
-                        ],
-                      ),
-                      Divider(),
-                      Container(
-                        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
-                        child: Column(
-                          children: [
-                            TagList(restaurant: restaurant),
-                            SizedBox(height: 10,),
-                            Info(kind: InfoKind.address, info: "경기 남양주시 화도읍 비룡로 145 1층",),
-                            SizedBox(height: 10,),
-                            Info(kind: InfoKind.phoneNumber, info: "031-511-9226",),
-                          ],
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Text("지도"),
-                          Text("공유하기"),
-                        ],
-                      )
+                      Title(restaurant: restaurant),
+                      Divider(thickness: 0.5,),
+                      DetailInfo(restaurant: restaurant),
+                      ActionButtons(),
+                      Divider(thickness: 0.5,),
+                      Reviews(),
                     ],
                   ),
                 ),
-                Text('Name: ${restaurant.name}'),
-                Text('ID: ${restaurant.restaurantId}'),
-                Text('Tags: ${restaurant.tags.join(', ')}'),
               ],
             );
           } else {
             return Text('No data');
           }
         },
+      ),
+    );
+  }
+}
+
+class Reviews extends StatelessWidget {
+  const Reviews({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [Align(alignment: Alignment.topLeft, child: Text("리뷰",  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),))],
+    );
+  }
+}
+
+class ActionButtons extends StatelessWidget {
+  const ActionButtons({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IntrinsicHeight(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          TextButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Text("지도",
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold))
+                ],
+              )),
+          VerticalDivider(
+            indent: 8,
+            endIndent: 8,
+            thickness: 0.5,
+          ),
+          TextButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Icon(Icons.ios_share_outlined),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text("공유",
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold))
+                ],
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+class DetailInfo extends StatelessWidget {
+  const DetailInfo({
+    super.key,
+    required this.restaurant,
+  });
+
+  final RestaurantDetail restaurant;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+      child: Column(
+        children: [
+          TagList(restaurant: restaurant),
+          SizedBox(
+            height: 10,
+          ),
+          Info(
+            kind: InfoKind.address,
+            info: "경기 남양주시 화도읍 비룡로 145 1층",
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Info(
+            kind: InfoKind.phoneNumber,
+            info: "031-511-9226",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    super.key,
+    required this.restaurant,
+  });
+
+  final RestaurantDetail restaurant;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              restaurant.name,
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            children: [
+              Rating(),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                '리뷰 157개',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
@@ -150,20 +242,15 @@ class TagList extends StatelessWidget {
         children: [
           for (Tag tag in restaurant.tags)
             Container(
-              padding: EdgeInsets.symmetric(
-                  horizontal: 5.0, vertical: 0.5),
+              padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 0.5),
               decoration: BoxDecoration(
-                  color:
-                  Theme.of(context).colorScheme.primary,
-                  border: Border.all(
-                      color: Theme.of(context)
-                          .colorScheme
-                          .primary),
+                  color: Theme.of(context).colorScheme.primary,
+                  border:
+                      Border.all(color: Theme.of(context).colorScheme.primary),
                   borderRadius: BorderRadius.circular(16.0)),
               child: Text(
                 "# ${tag.name}",
-                style: TextStyle(
-                    color: Colors.white, fontSize: 20),
+                style: TextStyle(color: Colors.white, fontSize: 20),
               ),
             ),
         ],
@@ -175,14 +262,17 @@ class TagList extends StatelessWidget {
 class Info extends StatelessWidget {
   final String kind;
   final String info;
+
   const Info({
-    super.key, required this.kind, required this.info,
+    super.key,
+    required this.kind,
+    required this.info,
   });
 
   getIcon(String kind) {
-    if(kind == InfoKind.address) {
+    if (kind == InfoKind.address) {
       return Icons.place;
-    } else if(kind == InfoKind.phoneNumber) {
+    } else if (kind == InfoKind.phoneNumber) {
       return Icons.phone_iphone;
     }
   }
@@ -200,8 +290,7 @@ class Info extends StatelessWidget {
         ),
         Text(
           info,
-          style: TextStyle(
-              fontSize: 18, fontWeight: FontWeight.normal),
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),
         ),
       ],
     );
