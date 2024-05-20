78ade8e1a7087c5054b2d8046b4db1d59ef04b9e
koomin1227 <koomin1227@naver.com>
2024-05-20 19:54:19 +0900

    Feat: 스크롤을 내리면 앱바가 축소되고 식당이름이 앱바에 나타나는 기능 구현

--- lib/screens/restaurant_datail_page.dart
+++ lib/screens/restaurant_datail_page.dart
@@ -19,6 +19,7 @@ class RestaurantDetailPage extends StatefulWidget {
 class _RestaurantDetailPageState extends State<RestaurantDetailPage>
     with TickerProviderStateMixin {
   late Future<RestaurantDetail> restaurantDetail;
+  var appBarTitle = "";
 
   Future<RestaurantDetail> fetchData() async {
     Response res =
@@ -40,9 +41,6 @@ class _RestaurantDetailPageState extends State<RestaurantDetailPage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-      ),
       body: FutureBuilder<RestaurantDetail>(
         future: restaurantDetail,
         builder:
@@ -53,46 +51,83 @@ class _RestaurantDetailPageState extends State<RestaurantDetailPage>
             return Text('Error: ${snapshot.error}');
           } else if (snapshot.hasData) {
             var restaurant = snapshot.data!;
-            return SingleChildScrollView(
-              child: Column(
-                children: [
-                  LayoutBuilder(
-                    builder:
-                        (BuildContext context, BoxConstraints constraints) {
-                      double width = constraints.maxWidth *
-                          0.8; // Image width is 80% of the available width
-                      double aspectRatio = 345 / 222; // Original aspect ratio
-                      return AspectRatio(
-                        aspectRatio: aspectRatio,
-                        child: Image.network(
-                          restaurant.thumbnail,
-                          width: width,
-                          fit: BoxFit.cover,
-                        ),
+            return CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  expandedHeight: 200.0,
+                  floating: false,
+                  pinned: true,
+                  iconTheme: IconThemeData(
+                    color: Colors.white,
+                  ),
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  flexibleSpace: LayoutBuilder(
+                    builder: (BuildContext context, BoxConstraints constraints) {
+                      var top = constraints.biggest.height;
+                      return FlexibleSpaceBar(
+                        title: top <= 120 ? Text(restaurant.name, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),) : null,
+                        background: RestaurantMainImage(restaurant: restaurant),
+                        collapseMode: CollapseMode.parallax,
                       );
                     },
                   ),
-                  Container(
-                    padding: EdgeInsets.all(15),
-                    child: Column(
-                      children: [
-                        Title(restaurant: restaurant),
-                        Divider(thickness: 0.5),
-                        DetailInfo(restaurant: restaurant),
-                        ActionButtons(),
-                        Divider(thickness: 0.5),
-                        Reviews(reviews: restaurant.reviews,),
-                      ],
-                    ),
+                ),
+                SliverToBoxAdapter(
+                  child: Column(
+                    children: [
+                      // RestaurantMainImage(restaurant: restaurant),
+                      Container(
+                        padding: EdgeInsets.all(15),
+                        child: Column(
+                          children: [
+                            Title(restaurant: restaurant),
+                            Divider(thickness: 0.5),
+                            DetailInfo(restaurant: restaurant),
+                            ActionButtons(),
+                            Divider(thickness: 0.5),
+                            Reviews(reviews: restaurant.reviews,),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                )
+              ],
             );
           } else {
             return Text('No data');
           }
         },
       ),
+    );
+  }
+}
+
+class RestaurantMainImage extends StatelessWidget {
+  const RestaurantMainImage({
+    super.key,
+    required this.restaurant,
+  });
+
+  final RestaurantDetail restaurant;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder:
+          (BuildContext context, BoxConstraints constraints) {
+        double width = constraints.maxWidth *
+            0.8; // Image width is 80% of the available width
+        double aspectRatio = 345 / 250; // Original aspect ratio
+        return AspectRatio(
+          aspectRatio: aspectRatio,
+          child: Image.network(
+            restaurant.thumbnail,
+            width: width,
+            fit: BoxFit.cover,
+          ),
+        );
+      },
     );
   }
 }
